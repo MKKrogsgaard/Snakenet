b02ec52db8a1b0c27709396b27e6b6dc49622e3d
Focus9752 <marcuskrogsgaard@outlook.de>
2025-11-30 13:18:46 +0100

    Refactored the game logic into its own class

--- src/main.py
+++ src/main.py
@@ -36,143 +36,150 @@ GAME_OVER_FONT_SIZE = 25
 X_POS_INITIAL = 0
 Y_POS_INITIAL = random.randint(0, 29)*GRID_SIZE
 
-snake = Snake(
-    body_color=SNAKE_BODY_COLOR,
-    head_color=SNAKE_HEAD_COLOR, 
-    x_pos_initial=X_POS_INITIAL, 
-    y_pos_initial=Y_POS_INITIAL,
-    grid_size=GRID_SIZE, 
-    min_x=0, 
-    max_x=WINDOW_SIZE_X - GRID_SIZE, 
-    min_y=0, 
-    max_y=WINDOW_SIZE_Y - GRID_SIZE)
+class Game():
+    def __init__(self):
+        self.snake = Snake(
+            body_color=SNAKE_BODY_COLOR,
+            head_color=SNAKE_HEAD_COLOR, 
+            x_pos_initial=X_POS_INITIAL, 
+            y_pos_initial=Y_POS_INITIAL,
+            grid_size=GRID_SIZE, 
+            min_x=0, 
+            max_x=WINDOW_SIZE_X - GRID_SIZE, 
+            min_y=0, 
+            max_y=WINDOW_SIZE_Y - GRID_SIZE)
 
-# Instantiate the apple
-apple = Apple(
-    color=APPLE_COLOR, 
-        x_pos_initial=0, 
-        y_pos_initial=0, 
-        grid_size=GRID_SIZE
-)
-
-apple.respawn()
-
-# Pygame setup, returns a tuple with the number of successfull and failed inits
-n_successful, n_errors  = pg.init()
-if n_errors > 0:
-    print(f'[!] Encountered {n_errors} error(s) when running pygame.init(), aborting...')
-else:
-    print('[+] Game initialized successfully!')
-
-pg.display.set_caption("Snakenet")
-screen = pg.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
-clock = pg.time.Clock()
-
-def showScore(score, color, font, font_size):
-    score_font = pg.font.SysFont(font, font_size)
-
-    score_surface = score_font.render('Score: ' + str(score), True, color)
-
-    score_rect = score_surface.get_rect()
-    
-    # Displays the text
-    screen.blit(score_surface, score_rect)
-
-def gameOver(score, color, font, font_size):
-    game_over_font = pg.font.SysFont(font, font_size)
-
-    game_over_surface = game_over_font.render('Game over. Final score: ' + str(score), True, color)
-
-    game_over_rect = game_over_surface.get_rect()
-
-    game_over_rect.midtop = (WINDOW_SIZE_X/2, WINDOW_SIZE_Y/4)
-
-    screen.blit(game_over_surface, game_over_rect)
-    pg.display.update()
-
-    print('[+] Game over!')
-
-    time.sleep(2)
-    pg.quit()
-
-    quit(0)
+        # Instantiate the apple
+        self.apple = Apple(
+            color=APPLE_COLOR, 
+            x_pos_initial=0, 
+            y_pos_initial=0, 
+            grid_size=GRID_SIZE
+        )
+        self.apple.respawn()
 
 
-game_is_running = True
-accumulated_time = 0
-temp_direction = snake.direction
+    def showScore(self, score, color, font, font_size):
+        score_font = pg.font.SysFont(font, font_size)
 
-while game_is_running:
-    deltaTime = clock.tick(GAME_FPS) / 1000 # In seconds
-    accumulated_time += deltaTime
+        score_surface = score_font.render('Score: ' + str(score), True, color)
 
-    logic_time_interval = 1 / SNAKE_MOVES_PER_SECOND # How long should a logical tick be for the current frame
+        score_rect = score_surface.get_rect()
+        
+        # Displays the text
+        self.screen.blit(score_surface, score_rect)
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            game_is_running = False
+    def gameOver(self, score, color, font, font_size):
+        game_over_font = pg.font.SysFont(font, font_size)
 
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_UP:
-                temp_direction = 'UP'
-            elif event.key == pg.K_DOWN:
-                temp_direction = 'DOWN'
-            elif event.key == pg.K_LEFT:
-                temp_direction = 'LEFT'
-            elif event.key == pg.K_RIGHT:
-                temp_direction = 'RIGHT'
+        game_over_surface = game_over_font.render('Game over. Final score: ' + str(score), True, color)
 
-    # Logical tick loop
-    while accumulated_time >= logic_time_interval:
+        game_over_rect = game_over_surface.get_rect()
+
+        game_over_rect.midtop = (WINDOW_SIZE_X/2, WINDOW_SIZE_Y/4)
+
+        self.screen.blit(game_over_surface, game_over_rect)
+        pg.display.update()
+
+        print('[+] Game over!')
+
+        time.sleep(2)
+        pg.quit()
+
+        quit(0) # TODO: return some stats instead of just quitting
+
+    def render(self):
+        # Drawing stuff
+        # Clear screen
+        self.screen.fill(BACKGROUND_COLOR)
+        pg.draw.rect(surface=self.screen, color=BORDER_COLOR, rect=border_rect, width=1)
+
+        self.showScore(
+                score=self.snake.score,
+                color=SCORE_TEXT_COLOR,
+                font=SCORE_FONT,
+                font_size=SCORE_FONT_SIZE
+            )
+
+        # Draw apple
+        pg.draw.rect(surface=self.screen, color=APPLE_COLOR, rect=(self.apple.position[0], self.apple.position[1], GRID_SIZE, GRID_SIZE))
+
+        # Draw snake
+        pg.draw.rect(surface=self.screen, color=SNAKE_HEAD_COLOR, rect=(self.snake.position[0], self.snake.position[1], GRID_SIZE, GRID_SIZE))
+        for pos in self.snake.tail:
+            pg.draw.rect(surface=self.screen, color=SNAKE_BODY_COLOR, rect=(pos[0], pos[1], GRID_SIZE, GRID_SIZE))
+
+        pg.display.update()
+
+    def processInput(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.game_is_running = False
+
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.temp_direction = 'UP'
+                elif event.key == pg.K_DOWN:
+                    self.temp_direction = 'DOWN'
+                elif event.key == pg.K_LEFT:
+                    self.temp_direction = 'LEFT'
+                elif event.key == pg.K_RIGHT:
+                    self.temp_direction = 'RIGHT'
+
+    def update(self):
         # Check if the player wants to move in the opposite direction of the last movement made by the snake
         # and stop the player from moving the snake inside its own body
-        if temp_direction == 'UP' and snake.direction != 'DOWN':
-            snake.direction = 'UP'
-        elif temp_direction == 'DOWN' and snake.direction != 'UP':
-            snake.direction = 'DOWN'
-        elif temp_direction == 'LEFT' and snake.direction != 'RIGHT':
-            snake.direction = 'LEFT'
-        elif temp_direction == 'RIGHT' and snake.direction != 'LEFT':
-            snake.direction = 'RIGHT'
+        if self.temp_direction == 'UP' and self.snake.direction != 'DOWN':
+            self.snake.direction = 'UP'
+        elif self.temp_direction == 'DOWN' and self.snake.direction != 'UP':
+            self.snake.direction = 'DOWN'
+        elif self.temp_direction == 'LEFT' and self.snake.direction != 'RIGHT':
+            self.snake.direction = 'LEFT'
+        elif self.temp_direction == 'RIGHT' and self.snake.direction != 'LEFT':
+            self.snake.direction = 'RIGHT'
 
-        snake.move()
+        self.snake.move()
 
-        if snake.isOutOfBounds() or snake.isBitingTail():
-            gameOver(
-                score=snake.score,
+        if self.snake.isOutOfBounds() or self.snake.isBitingTail():
+            self.gameOver(
+                score=self.snake.score,
                 color=GAME_OVER_TEXT_COLOR,
                 font=GAME_OVER_FONT,
                 font_size=GAME_OVER_FONT_SIZE
             )
 
-        if snake.position == apple.position:
-            snake.score += 1
-            snake.grow()
-            apple.respawn()
+        if self.snake.position == self.apple.position:
+            self.snake.score += 1
+            self.snake.grow()
+            self.apple.respawn()
 
-        accumulated_time -= logic_time_interval
-        
-    # Drawing stuff
-    # Clear screen
-    screen.fill(BACKGROUND_COLOR)
-    pg.draw.rect(surface=screen, color=BORDER_COLOR, rect=border_rect, width=1)
+        self.accumulated_time -= self.logic_time_interval
 
-    showScore(
-            score=snake.score,
-            color=SCORE_TEXT_COLOR,
-            font=SCORE_FONT,
-            font_size=SCORE_FONT_SIZE
-        )
+    def startGame(self):
+        # Pygame setup, returns a tuple with the number of successfull and failed inits
+        n_successful, n_errors  = pg.init()
+        if n_errors > 0:
+            print(f'[!] Encountered {n_errors} error(s) when running pygame.init(), aborting...')
+        else:
+            print('[+] Game initialized successfully!')
 
-    # Draw apple
-    pg.draw.rect(surface=screen, color=APPLE_COLOR, rect=(apple.position[0], apple.position[1], GRID_SIZE, GRID_SIZE))
+        self.screen = pg.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
+        self.clock = pg.time.Clock()
 
-    # Draw snake
-    pg.draw.rect(surface=screen, color=SNAKE_HEAD_COLOR, rect=(snake.position[0], snake.position[1], GRID_SIZE, GRID_SIZE))
-    for pos in snake.tail:
-        pg.draw.rect(surface=screen, color=SNAKE_BODY_COLOR, rect=(pos[0], pos[1], GRID_SIZE, GRID_SIZE))
+        self.game_is_running = True
+        self.accumulated_time = 0
+        self.temp_direction = self.snake.direction
 
-    pg.display.update()
+        while self.game_is_running:
+            deltaTime = self.clock.tick(GAME_FPS) / 1000 # In seconds
+            self.accumulated_time += deltaTime
 
+            self.logic_time_interval = 1 / SNAKE_MOVES_PER_SECOND # How long should a logical tick be for the current frame
 
+            self.processInput()
+            while self.accumulated_time >= self.logic_time_interval:
+                self.update()
+                self.render()
 
+game = Game()
+game.startGame()
